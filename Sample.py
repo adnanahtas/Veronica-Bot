b88c2bb845219a8de82965f8a7d4b10e67999140
siddharthnale <siddharthnale03@gmail.com>
2024-01-26 21:38:45 +0530

    created functions

--- Sample.py
+++ Sample.py
@@ -2,19 +2,28 @@ from ctransformers import AutoModelForCausalLM, AutoConfig
 import time
 
 t = time.time()
-role = "Professional Storyteller"
-question = "Write an essay on cheetahs in 200 words"
+role = "Professor"
+question = "What is Life"
 
-config = AutoConfig.from_pretrained("TheBloke/Mistral-7B-OpenOrca-GGUF")
-config.config.gpu_layers = 50
-config.config.mlock = True
-config.config.context_length = 4096
-config.config.batch_size = 16
-config.config.max_new_tokens = 8192
-llm = AutoModelForCausalLM.from_pretrained("TheBloke/Mistral-7B-Instruct-v0.1-GGUF", model_file="mistral-7b-instruct-v0.1.Q5_K_M.gguf", model_type="mistral", config=config)
+model_repo = "TheBloke/Mistral-7B-Instruct-v0.2-GGUF"
 
-prompt = f"<s>[INST]You are a {role} named Veronica. You will respond in a way that is most natural and relevant for a {role}, while making sure to follow any specific instructions given to you[/INST]Sure, I will now respond to the conversation as if I am a {role} and my answers will be context relevant and related to my role of being a {role}</s>[INST]{question}[/INST]"
-response = llm(prompt)
-print(response)
+def init_model():
+    config = AutoConfig.from_pretrained("TheBloke/Mistral-7B-Instruct-v0.2-GGUF")
+    config.config.gpu_layers = 50
+    # config.config.mlock = True
+    config.config.context_length = 256
+    config.config.batch_size = 8
+    config.config.max_new_tokens = 1024
+    llm = AutoModelForCausalLM.from_pretrained(model_repo, model_file="mistral-7b-instruct-v0.2.Q5_K_M.gguf", model_type="mistral", config=config)
+    return llm
+
+def generate(model, role, question):
+    prompt = f"<s>[INST]You are a {role} named Veronica. You will respond in a way that is most natural and relevant for a {role}[/INST]Sure, I will now respond to the conversation as if I am a {role} and my answers will be context relevant and related to my role of being a {role}</s>[INST]{question}[/INST]"
+    response = model(prompt)
+    return response
+
+m = init_model()
+op = generate(m, role, question)
+print(op)
 t = time.time() - t
 print("Time = ", t)
