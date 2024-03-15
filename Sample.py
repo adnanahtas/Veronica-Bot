a50f6025f21e4859576b58264e990d30515c17be
siddharthnale <siddharthnale03@gmail.com>
2024-03-15 21:18:43 +0530

    hebe push

--- Sample.py
+++ Sample.py
@@ -2,19 +2,21 @@ from ctransformers import AutoModelForCausalLM, AutoConfig
 import time
 
 t = time.time()
-role = "Professor"
-question = "What is Life"
+role = "personal assistant"
+question = "openAI's "
 
 model_repo = "TheBloke/Mistral-7B-Instruct-v0.2-GGUF"
 
 def init_model():
-    config = AutoConfig.from_pretrained("TheBloke/Mistral-7B-Instruct-v0.2-GGUF")
+    config = AutoConfig.from_pretrained("TheBloke/Mistral-7B-Instruct-v0.1-GGUF")
     config.config.gpu_layers = 50
     # config.config.mlock = True
-    config.config.context_length = 256
-    config.config.batch_size = 8
+    config.config.context_length = 2048
+    config.config.batch_size = 16
     config.config.max_new_tokens = 1024
-    llm = AutoModelForCausalLM.from_pretrained(model_repo, model_file="mistral-7b-instruct-v0.2.Q5_K_M.gguf", model_type="mistral", config=config)
+    llm = AutoModelForCausalLM.from_pretrained("TheBloke/Mistral-7B-Instruct-v0.1-GGUF", model_file="mistral-7b-instruct-v0.1.Q5_K_M.gguf", model_type="mistral", config=config)
+    llm._context = llm.tokenize("The F5 and the F5 Pro are almost identical in size and core features, but upon closer look at the specs sheet, we start to see how they differ in some niche aspects. Still, both phones are aimed towards the budget-conscious crowd, with the F5 Pro acting as an affordable flagship. The handset offers a few notable upgrades over its predecessor while maintaining the same price point (the F4 launched for €400). It's a great choice if you are looking for a capable midranger with some upper-tier features on the side. The standard F5 runs on a brand new Snapdragon 7+ Gen 2 chipset, which hopefully isn't just a rebranded Snapdragon 778G yet again. The handset also features a familiar 6.67-inch OLED panel with a 120Hz refresh rate and high maximum brightness as its more expensive sibling but at a lower resolution. The camera setups of both devices are identical, and the battery capacity is rated at around 5,000 mAh with support for blazing-fast 67W fast charging. The only difference is that the F5 Pro adds wireless charging to its list of features. It seems like no matter which device you decide to get, the user experience won't vary by much. But if wireless charging and a more future-proof SoC, such as the Snapdragon 8+ Gen 1, are up on your priority list, the few extra bucks for the F5 Pro might be worth it. Still, we believe that the F5 will get you 90% of where you want to be, making it presumably the better deal of the two. ")
+    print("model load successful!")
     return llm
 
 def generate(model, role, question):
